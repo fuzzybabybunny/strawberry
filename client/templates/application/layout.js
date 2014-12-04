326c23ea98983d83c96fe999383f8f04bda691a1
Gene Lim <zijin.lim@gmail.com>
2014-12-04 23:53:03 +0800

    work on scrolling animation

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -14,4 +14,53 @@ $(function () {
     return false;
   });
 
+  $(".dropdown-toggle").on('click', function(event) {
+    event.preventDefault();
+    if ($(".dropdown").hasClass('slided') == false) {
+      $(".dropdown").addClass('slided');
+      $(".dropdown-menu").slideDown('fast')
+    } else {
+      $(".dropdown").removeClass('slided');
+      $(".dropdown-menu").slideUp('fast')
+
+    }
+    return false;
+  });
+
+
+  // $( window ).scroll(function() {
+  //   $.data( this, "scrollCheck", setTimeout(function() {
+  //     $( "div.navbar.navbar-info.navbar-fixed-top" ).removeClass('box-shadow');
+  //   }, 2500) );
+  // });
+
+  var CurrentScroll = 0;
+  $(window).scroll(function(event){
+    $( "div.navbar.navbar-info.navbar-fixed-top" ).addClass('box-shadow');
+    var NextScroll = $(this).scrollTop();
+    if (NextScroll > CurrentScroll){
+
+      //write the codes related to down-ward scrolling here
+      if (window.scrollY >= 300) {
+        $( "div.navbar.navbar-info.navbar-fixed-top" ).fadeOut(600);
+      }
+
+    } else {
+
+      $( "div.navbar.navbar-info.navbar-fixed-top" ).fadeIn(200);
+
+      //write the codes related to upward-scrolling here
+      // $( "div.navbar.navbar-info.navbar-fixed-top" ).fadeIn("fast");
+    }
+    //Updates current scroll position
+    CurrentScroll = NextScroll;
+  });
+
+  // if (window.scrollY >= 300) {
+  //     $( "div.navbar.navbar-info.navbar-fixed-top" ).delay(2000),fadeOut(1000);
+  //   } else if (window.scrollY == 0) {
+  //     $( "div.navbar.navbar-info.navbar-fixed-top" ).fadeIn(1000);
+  //   }
+  // }
+
 })
