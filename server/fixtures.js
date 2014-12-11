4a8705e7c0cac111918774e0f544d3027c411db5
kelvinblade <kelvinblade@gmail.com>
2014-12-11 14:28:14 +0800

    Change seed, add category

--- server/fixtures.js
+++ server/fixtures.js
@@ -13,21 +13,43 @@ if (Comments.find().count() === 0) {
   });
   var fer = Meteor.users.findOne(ferId);
 
-  var videoId = Videos.insert({
+  var video1Id = Videos.insert({
     userId: mark._id,
     author: mark.profile.name,
     createdAt: new Date(),
     videoSourceId: "LdH1hSWGFGU",
-    videoTitle: "Woman playing piano"
+    videoTitle: "Woman playing piano",
+    categoryTopVote : ["lmao"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
   });
-  var video = Videos.findOne(videoId);
+  var video = Videos.findOne(video1Id);
 
   var video2Id = Videos.insert({
     userId: mark._id,
     author: mark.profile.name,
     createdAt: new Date(),
     videoSourceId: "VxvDVhjALoU",
-    videoTitle: "Funny Video 1"
+    videoTitle: "Funny Video 1",
+    categoryTopVote : ["love"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
   });
   var video = Videos.findOne(video2Id);
 
@@ -35,10 +57,141 @@ if (Comments.find().count() === 0) {
     userId: mark._id,
     author: mark.profile.name,
     createdAt: new Date(),
-    videoSourceId: "8QOSztE",
-    videoTitle: "Funny Video 2"
+    videoSourceId: "no1q3wuvwmg",
+    videoTitle: "bum implant",
+    categoryTopVote : ["fku"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
   });
   var video = Videos.findOne(video3Id);
+
+  var video4Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "nIsCs9_-LP8",
+    videoTitle: "Emotional baby! Too cute!",
+    categoryTopVote : ["magic"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video3Id);
+
+  var video5Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "NIZmH96vUhU",
+    videoTitle: "Câmera Escondida Annabelle - Inédita (05/10/14) - Annabelle Prank",
+    categoryTopVote : ["omfg"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video3Id);
+
+  var video6Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "fA7XfJAPTLQ",
+    videoTitle: "Cheating Death - Near Death Experiences",
+    categoryTopVote : ["shite"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video3Id);
+
+  var video7Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "wWLhrHVySgA",
+    videoTitle: "Alison Gold - Chinese Food (Official Music Video)",
+    categoryTopVote : ["nsfw"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video3Id);
+
+  var video8Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "dTAAsCNK7RA",
+    videoTitle: "OK Go - Here It Goes Again",
+    categoryTopVote : ["die"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video3Id);
+
+  var video9Id = Videos.insert({
+    userId: mark._id,
+    author: mark.profile.name,
+    createdAt: new Date(),
+    videoSourceId: "6bVa6jn4rpE",
+    videoTitle: "University of Florida student Tasered at Kerry forum",
+    categoryTopVote : ["lmao","love"],
+    categoryVotes : {
+      lmao: 0,
+      love: 0,
+      fku: 0,
+      magic: 0,
+      omfg: 0,
+      shite: 0,
+      nsfw: 0,
+      die: 0
+    }
+  });
+  var video = Videos.findOne(video9Id);
 
   Comments.insert({
     videoId: video._id,
