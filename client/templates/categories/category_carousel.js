5194d1f3ea5c109b1feab5e8de36f740f6743cfd
Gene Lim <zijin.lim@gmail.com>
2014-12-11 20:12:53 +0800

    set up carousel template and category backend linkage

--- client/templates/categories/category_carousel.js
+++ client/templates/categories/category_carousel.js
@@ -1,3 +1,18 @@
+// Template.categoryCarousel.helpers({
+//   categories: function() {
+//     return Videos.find({}, {sort: {createdAt: -1}});
+//   }
+// });
+
 Template.categoryCarousel.rendered = function () {
 
 };
+
+Template.categoryCarousel.helpers({
+  'videos': function(){
+    console.log(this);
+    videos = Videos.find({categoryTopVote: this.foo}).fetch();
+    return videos;
+  }
+
+});
